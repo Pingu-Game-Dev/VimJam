277d268af4168479c9bb6c3de43845e04bb29719
MarkAHarley <markharley100@gmail.com>
2020-09-20 00:29:10 +0100

    Removed unnecessary code in death.cs

--- VimJam/Assets/Death.cs
+++ VimJam/Assets/Death.cs
@@ -6,22 +6,11 @@ using UnityEngine;
 
 public class Death : MonoBehaviour {
 
-    public static Vector3 respawnPoint = new Vector3(-50f,12f);
+    public static Vector2 respawnPoint = new Vector2(-50f,12f);
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         collision.transform.position = respawnPoint;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
