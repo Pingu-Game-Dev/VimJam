293b7fa3fe9b0f329980df2edcb900feb28b9898
Mark Harley <2378551h@student.gla.ac.uk>
2020-09-23 01:33:22 +0100

    Started End Level script

--- VimJam/Assets/Scripts/EndLevel.cs
+++ VimJam/Assets/Scripts/EndLevel.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class EndLevel : MonoBehaviour
 {
     public GameObject music;
+    private Component[] sources;
+    private GameObject player;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.GetType() == new BoxCollider2D().GetType()){
+            sources = music.GetComponentsInChildren<AudioSource>();
+            player = collision.collider.gameObject;
+
+            foreach (AudioSource source in sources){
+                source.enabled = false;
+            }
+
+            player.GetComponent<PlayerMovement>().enabled = false;
+        }
     }
 }
