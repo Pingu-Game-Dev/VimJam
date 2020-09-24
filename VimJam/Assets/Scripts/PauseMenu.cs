02e59f46a9847d7b04392cb1cf6af7faf674f67a
MHCooke <grapesdigital@gmail.com>
2020-09-24 01:55:07 +0100

    Revert "commit for revert"
    This reverts commit 9f1bb0f64d86866b67b31c5ac8c03779ef4bfb65.

--- VimJam/Assets/Scripts/PauseMenu.cs
+++ VimJam/Assets/Scripts/PauseMenu.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
@@ -54,61 +53,4 @@ public class PauseMenu : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false;
     }
 
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using UnityEngine;
-
-public class PauseMenu : MonoBehaviour
-{
-
-    public static bool GameIsPaused = false;
-
-    public GameObject pauseMenuUI;
-
-    public AudioLowPassFilter lp;
-
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-                resume();
-            }
-            else
-            {
-                pause();
-            }
-        }
-    }
-
-    public void resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1;
-        lp.enabled = false;
-        GameIsPaused = false;
-    }
-
-    void pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0;
-        GameIsPaused = true;
-        lp.enabled = true;
-    }
-
-    public void quitgame()
-    {
-        Debug.Log("Quitting Game");
-        Application.Quit();
-        UnityEditor.EditorApplication.isPlaying = false;
-    }
-
->>>>>>> parent of a872662... Manual merge
 }
