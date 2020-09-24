31d314576bc2d2716127e89d1b965426b104b0ab
MarkAHarley <markharley100@gmail.com>
2020-09-24 18:58:28 +0100

    Added Car Enemy Script

---
+++ VimJam/Assets/Scripts/CarEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEnemy : MonoBehaviour
+{
+    private float timer = 0f;
+    public float speed = 3f;
+
+    public float deathTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= deathTime && gameObject.GetComponent<BoxCollider2D>().enabled){
+            timer = 0f;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+
+        if (timer >= speed){
+            timer = 0f;
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
